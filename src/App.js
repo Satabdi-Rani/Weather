3428959d2078c08d56640dc5a7125788263bf32b
Satabdi Rani <32415291+Satabdi-Rani@users.noreply.github.com>
2023-08-01 16:59:58 +0600

    Edited

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+  // const url =`https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=9dc5f4e2537673766aa2c36f9457fc8f`;
   return (
     <div className="App">
-       
+       <div className="container">
+          <div className="top">
+              <div className="location">
+                  <p>Dhaka</p>
+              </div>
+              <div className="temp">
+                  <h1>65F</h1>
+              </div> 
+          </div>
+        <div className="bottom"></div>
+       </div>
     
     </div>
   );
